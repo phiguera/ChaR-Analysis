442726e82bf49433b8b06f4256c58a7aeb2f7821
Philip Higuera <phiguera@uidaho.edu>
2014-12-19 13:48:44 -0800

    Changed main file name, and uploaded CharPretreatment.R
    (1) Changed file name for main file, and minor changes to text within.
    (2) Started working on translating CharPretretment.m into
    CharPretreatment.R

---
+++ R/CharAnalysis.R
@@ -0,0 +1,31 @@
+#' @title ChaR-Analysis
+#' @description Diagnostic and anlystical tools for peak analysis in 
+#' sediment-charcoal records. This code is based on original Matlab code, 
+#' CharAnalysis reads in a data object or a filename and uses it to 
+#' parameterize the detection of charcoal peaks from background values.
+#'
+#' @importFrom RJSONIO fromJSON
+#' @param x Optional parameter for a \code{site}, \code{dataset}, or \code{dataset_list}.
+#' @author Philip Higuera \email{phiguera@@uidaho.edu}
+#' @return This command returns either object of class \code{"try-error"}' (see \code{\link{try}}) definined by the error returned from the Neotoma API call, or an object of class \code{charanalysis}, containing a set of objects:
+#'
+#'  \item{ \code{charcoal} }{}
+#'  \item{ \code{char.thresh} }{}}
+#'  \item{ \code{pre.treatment} }{}
+#'  \item{ \code{smoothing} }{}
+#'  \item{ \code{peak.analysis} }{}
+#'  \item{ \code{results} }{}
+#'
+#' @section Note:
+#'
+#' @examples \dontrun{
+#'
+#' }
+#' @references
+#' CharAnalysis documentation: http://code.google.com/p/charanalysis/
+#' @keywords IO connection
+#' @export
+
+CharAnalysis <- function(x){
+  
+}
