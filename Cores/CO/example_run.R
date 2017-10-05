042a772efa429e7f4f24f4b8da324bb9e5904ecf
wfinsinger <walter.finsinger@um2.fr>
2017-10-05 11:40:49 +0200

    R script to run function with example record (CO_charData)

---
+++ Cores/CO/example_run.R
@@ -0,0 +1,10 @@
+rm(list=ls())
+
+# set working directory
+setwd('/Users/wfinsing/Documents/GitHub/ChaR-Analysis/Cores')
+
+# load CharAnalysis.R source file
+source('../R/CharAnalysis.R')
+
+# run function using CO_charData.csv
+CharAnalysis()
